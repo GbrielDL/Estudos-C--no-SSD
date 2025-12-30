4974714c63e32568c299664bfc5f16ccd75f0445
Gabriel de Lima Gonçalves <gabrielgonsalves508@gmail.com>
2025-12-29 21:14:17 -0300

    Mini projeto Pizzaria (completo)

--- Revision/Pizzaria/Functions.cs
+++ Revision/Pizzaria/Functions.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace pizzaria
 {
+    // Sabores Pizzas
     public enum Sabores{Bacon, Frango, Calabresa, Toscana, Nordestina};
+    // Bebidas
+    public enum Bebidas{CocaCola, Fanta, Guaraná, Sprite};
     public static class Functions
     {
         public static int LerInteiro(string mensagem)
@@ -11,14 +15,16 @@ namespace pizzaria
             int valor;
             while (true)
             {
+                // Ler mensagem do teclado
                 Console.Write(mensagem);
                 string entrada = Console.ReadLine();
-
+                // Verifica se o campo está vazio
                 if (string.IsNullOrWhiteSpace(entrada))
                 {
                     Console.WriteLine("Error! Campo vazio.");
                     continue;
                 }
+                // Verifica se é um número inteiro
                 if (!int.TryParse(entrada, out valor))
                 {
                     Console.WriteLine("Error! Digite um número inteiro.");
@@ -26,6 +32,105 @@ namespace pizzaria
                 }
                 return valor;
             }
+        }
+
+        public static string LerString(string mensagem)
+        {
+            while (true)
+            {
+                // Ler mensagem do teclado
+                Console.Write(mensagem);
+                string entrada = Console.ReadLine();
+                // Verificar se o campo está vazio
+                if (string.IsNullOrWhiteSpace(entrada))
+                {
+                    Console.WriteLine("Error! Campo vazio");
+                    continue;
+                }
+                // Verificar se realmente tem apenas Letras
+                bool apenasLetras = true;
+                foreach (var c in entrada)
+                {
+                    if (!char.IsLetter(c) && c != ' ')
+                    {
+                        apenasLetras = false;
+                        break;
+                    }
+                }
+                // Se não tiver apenas letras, repete tudo novamente
+                if (!apenasLetras)
+                {
+                    Console.WriteLine("Error! Digite apenas letras (A - Z)");
+                    continue;
+                }
+                return entrada;
+            }
+        }
+
+        public static char LerTamPizza(string mensagem)
+        {
+            while (true)
+            {
+                Console.Write(mensagem);
+                string entrada = Console.ReadLine().Trim().ToUpper();
+                // Verifica se digitou apenas 1 Caractere
+                if (entrada.Length != 1)
+                {
+                    Console.WriteLine("Error! Digite apenas um caractere");
+                    continue;
+                }
+
+                char tamanho = entrada[0];
+                // Verificar se é P, M ou G
+                if (tamanho != 'P' && tamanho != 'M' && tamanho != 'G')
+                {
+                    Console.WriteLine("Error! Digite um tamanho válido.");
+                    continue;
+                }
+
+                return tamanho;
+            }
+        }
+        // Calculando o preço da Pizza
+        public static double ValorPizza(Sabores sabor, char tamanho)
+        {
+            return (sabor, tamanho) switch
+            {
+                (Sabores.Bacon, 'P') => 24.4,
+                (Sabores.Bacon, 'M') => 30,
+                (Sabores.Bacon, 'G') => 45,
+
+                (Sabores.Calabresa, 'P') => 22,
+                (Sabores.Calabresa, 'M') => 28.5,
+                (Sabores.Calabresa, 'G') => 36.7,
+
+                (Sabores.Frango, 'P') => 18.5,
+                (Sabores.Frango, 'M') => 23,
+                (Sabores.Frango, 'G') => 29.9,
+
+                (Sabores.Nordestina, 'P') => 25,
+                (Sabores.Nordestina, 'M') => 30.4,
+                (Sabores.Nordestina, 'G') => 38.6,
+
+                (Sabores.Toscana, 'P') => 28,
+                (Sabores.Toscana, 'M') => 35,
+                (Sabores.Toscana, 'G') => 45,
+
+                _ => 0
+            };
+        }
+        // Calculando o preço da Bebida
+        public static double ValorBebida(Bebidas bebida)
+        {
+            return (bebida) switch
+            {
+                Bebidas.CocaCola => 7.5,
+                Bebidas.Fanta => 4.5,
+                Bebidas.Guaraná => 5,
+                Bebidas.Sprite => 3.5,
+
+                _ => 0
+            };
         }
     }
 }
--- Revision/Pizzaria/Program.cs
+++ Revision/Pizzaria/Program.cs
@@ -10,16 +10,69 @@ namespace pizzaria
         static void Main(string[] args)
         {
             int contador = 0;
-
+            //Escolhendo o Sabor da Pizza
             Console.WriteLine("--- Escolha o Sabor da Pizza ---");
             foreach (var sabor in Enum.GetNames(typeof(Sabores)))
             {
                 contador++;
                 Console.WriteLine($"{contador} - {sabor}");
             }
-            int escolha = LerInteiro("Opção: ");
+            // Verifica se a escolha está dentro das opções Sabores
+            int escolha;
+            while (true)
+            {
+                escolha = LerInteiro("Opção: ");
+                if (escolha > Enum.GetNames<Sabores>().Length || escolha < 1)
+                {
+                    Console.WriteLine("Escolha inválida.");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
 
-            Console.WriteLine($"Ótimo, você escolheu o sabor {(Sabores)escolha-1}");
+            //Escolhendo o Tamanho da Pizza
+            char tamanho = LerTamPizza("Agora escolha o tamanho [P, M, G]: ");
+
+            // Confirmando o pedido, e perguntando se quer bebida
+            double valor_pizza = ValorPizza((Sabores)escolha-1, tamanho);
+            Console.WriteLine($"Ótimo, você escolheu uma Pizza '{tamanho}' sabor {(Sabores)escolha-1}. Valor total {valor_pizza:c}");
+
+            Console.Write("Alguma coisa pra beber? [S/N]: ");
+            string resp = Console.ReadLine().ToUpper();
+            // Caso escolha algo pra beber, entrar no if
+            if (resp[0] != 'N')
+            {
+                int cont = 0;
+                Console.WriteLine("Fique a vontade: ");
+                foreach (var bebida in Enum.GetNames(typeof(Bebidas)))
+                {
+                    cont++;
+                    Console.WriteLine($"{cont} - {bebida}");
+                }
+                int esc_bebida;
+                while (true)
+                {
+                    esc_bebida = LerInteiro("Escolha: ");
+                    if (esc_bebida > Enum.GetNames<Bebidas>().Length || esc_bebida < 1)
+                    {
+                        Console.WriteLine("Escolha inválida.");
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                double valor_total = valor_pizza + ValorBebida((Bebidas)esc_bebida-1);
+                Console.Write($"--- Seu Pedido ---\n{(Sabores)escolha-1} '{tamanho}' {(Bebidas)esc_bebida-1,10}\nTotal: {valor_total:c}");
+            }
+            else
+            {
+                Console.Write($"--- Seu Pedido ---\n{(Sabores)escolha-1} '{tamanho}'\nTotal: {valor_pizza:c}");
+            }
         }
     }
 }
